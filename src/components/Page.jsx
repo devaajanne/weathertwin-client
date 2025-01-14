e9a3eaa240738302dbd681688acd3a4455c6c0dd
devaajanne <devaajanne@gmail.com>
2025-01-14 14:57:20 +0200

    Added imports and renamed components

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+
 import PageToolbar from "./PageToolBar";
+import WeatherLayout from "./pageWeatherDisplay/WeatherLayout";
 
 export default function Page() {
   return (
     <>
       <PageToolbar />
+      <WeatherLayout />
     </>
   );
 }
