2788dca2c4ee13d5f6cf0469fa826b2104174454
devaajanne <devaajanne@gmail.com>
2025-01-14 15:02:55 +0200

    Added a layout file for search bar and weather data

---
+++ src/components/pageWeatherDisplay/WeatherLayout.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import Grid from "@mui/material/Grid2";
+
+import WeatherSearchBar from "./WeatherSearchBar";
+import WeatherDisplay from "./WeatherDisplay";
+
+export default function WeatherLayout() {
+  const [inputLocation, setInputLocation] = useState(null);
+  const [similarLocation, setSimilarLocation] = useState(null);
+
+  return (
+    <>
+      <Grid container alignItems='center' justifyContent='center'>
+        <Grid item>
+          <WeatherSearchBar setInputLocation={setInputLocation} setSimilarLocation={setSimilarLocation} />
+        </Grid>
+      </Grid>
+      <Grid container alignItems='center' justifyContent='center'>
+        <Grid item>
+          <WeatherDisplay inputLocation={inputLocation} similarLocation={similarLocation} />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
