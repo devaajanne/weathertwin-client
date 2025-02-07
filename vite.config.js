30d754c646b8c98fc5772fabc7c6038ed8f39053
devaajanne <devaajanne@gmail.com>
2025-02-07 11:19:48 +0200

    Fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/weathertwin-client/",
+  base: '/weathertwin-client/',
   plugins: [react()],
 })
