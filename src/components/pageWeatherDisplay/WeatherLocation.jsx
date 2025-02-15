eb1bf87b4eb9688fdcbe48d4b1a423c632ebfc09
devaajanne <devaajanne@gmail.com>
2025-02-15 12:28:39 +0200

    Added temp unit symbol (°C or °F) to location cards

--- src/components/pageWeatherDisplay/WeatherLocation.jsx
+++ src/components/pageWeatherDisplay/WeatherLocation.jsx
@@ -10,7 +10,7 @@ export default function WeatherLocation({ location }) {
           <CardContent>
             <Typography variant='h5'>{location.city}</Typography>
             <Typography>{location.countryName}</Typography>
-            <Typography>{location.temp}</Typography>
+            <Typography>{location.temp} {location.tempUnit}</Typography>
             <Typography>{location.weatherGroup}</Typography>
           </CardContent>
         </Card>
